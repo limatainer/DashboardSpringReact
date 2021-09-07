4d50c1fa4582a35c3fc0d78513a9900d2bdaeb9a
Mariana Lima <limatainer@gmail.com>
2021-09-07 18:41:43 +0100

    :woman_technologist:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,34 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard</h1>
+        <br />
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Success Rate (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Movimentation Pieces</h5>
+            <DonutChart />
+          </div>
+          <br />
+          <div className="py-3">
+            <h2 className="text-primary">All Live Movimentations</h2>
+          </div>
+        </div>
         <br />
         <DataTable />
       </div>
+      <br />
       <Footer />
     </>
   );
