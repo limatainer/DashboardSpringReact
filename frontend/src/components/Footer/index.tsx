b424c84a6638bcf10004bf0fd1f5008c26e17c4b
Mariana Lima <limatainer@gmail.com>
2021-09-07 17:38:41 +0100

    new components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">Developed with Love by <a href="https://github.com/limatainer" target="_blank" rel="noreferrer">Mariana Cousseiro</a></p>
+        <p className="text-light"><small><strong>Spring React Week</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+
+
+  );
+}
+
+export default Footer;
